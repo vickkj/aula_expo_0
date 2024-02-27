f8c191f9c7ef79a89c89e7c743012fddc5a24288
Victor Hugo <135546677+vickkj@users.noreply.github.com>
2024-02-27 15:25:11 -0300

    aula 27-02-24

---
+++ component/exemplo_0/index.js
@@ -0,0 +1,9 @@
+import{View, Text} from 'react-native';
+function exemplo_0 (){
+ return (
+      <View>
+      <Text style="color:green">Minha primeira tela</Text>
+      </View>   
+ );
+}
+export default exemplo_0;
