e8e824f9d79f11226335934256bd7022117fd473
Victor Hugo <135546677+vickkj@users.noreply.github.com>
2024-03-19 15:25:56 -0300

    19-03-24

---
+++ component/Exemplo_2/index.js
@@ -0,0 +1,12 @@
+import { View,Text} from 'react-native';
+
+import styles from './style';
+
+function Exemplo_2 () {
+    return (
+    <View style={styles.container}>
+        <Text> Exemplo 2</Text>
+    </View>
+    )
+}
+export default Exemplo_2;
---
+++ component/Exemplo_2/style.js
@@ -0,0 +1,10 @@
+
+import { StyleSheet } from "react-native-web";
+
+const styles= StyleSheet.create({
+    container:{
+        backgroundColor: '#fff',
+    }
+});
+
+export default styles;
