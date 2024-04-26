298442fa40339580a59595c9e7fe21ddb4b389f0
VICTOR HUGO RODRIGUES ROSA <victor.rosa44>
2024-04-26 09:50:50 -0300

    sou burro

--- App.js
+++ App.js
@@ -3,12 +3,11 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import Exemplo_0 from './component/exemplo_1';
 import Exemplo_2 from './component/Exemplo_2/index.js';
-import atividade_2 from  './component/atividade_2/index.js';
+import atividade_2
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <atividade_2 />
       <StatusBar style="auto" />
     </View>
   );
